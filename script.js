73ca874fac0dffa7d023fca199041071984e8fde
Sjeakx <issuers_grad_0v@icloud.com>
2024-10-17 20:36:49 -0700

    sigma

--- script.js
+++
@@ -1,5 +0,0 @@
-// Open a new blank window
-let newWindow = window.open('about:blank', '_blank');
-
-// Set a URL for the new window
-newWindow.location.href = 'https://example.com'; // replace with the desired URL
